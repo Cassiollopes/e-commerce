4dfe3bbda7edfb84cd9635b54fba0bfc44419dd5
cassio <cassio8976@gmail.com>
2025-01-20 18:57:11 -0300

    modified:   src/app/dashboard/page.tsx 	modified:   src/components/dashboard/last-sales.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -25,3 +25,5 @@ export default function Page() {
     </div>
   )
 }
+
+export const dynamic = 'force-dynamic'
--- src/components/dashboard/last-sales.tsx
+++ src/components/dashboard/last-sales.tsx
@@ -23,5 +23,3 @@ export default async function LastSales() {
     </div>
   );
 }
-
-export const dynamic = 'force-dynamic'
