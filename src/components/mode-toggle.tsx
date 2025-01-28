6ef36fcc9329a8fa6e7201fcccf341fa2a902e11
cassio <cassio8976@gmail.com>
2025-01-28 17:37:28 -0300

    modified:   src/components/log-button.tsx 	modified:   src/components/mode-toggle.tsx

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -10,7 +10,7 @@ export default function ModeToggle() {
   const { setTheme } = useTheme()
 
   return (
-    <Button variant="outline" size="icon" onClick={() => document.documentElement.classList.contains("dark") ? setTheme("light") : setTheme("dark")} className="group h-11 w-11">
+    <Button variant="outline" size="icon" onClick={() => document.documentElement.classList.contains("dark") ? setTheme("light") : setTheme("dark")} className="group h-10 w-10">
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 dark:group-hover:rotate-90 dark:group-hover:scale-100 group-hover:rotate-90 group-hover:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:group-hover:rotate-0 dark:group-hover:scale-0 group-hover:rotate-0 group-hover:scale-100" />
     </Button>
