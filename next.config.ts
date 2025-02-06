74cc0856f778e4ce6630a69230a8e7094d56fa47
cassio <cassio8976@gmail.com>
2025-02-06 16:52:23 -0300

    modified:   next.config.ts 	new file:   src/app/error.tsx 	modified:   src/lib/data.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "i.ibb.co", "lh3.googleusercontent.com"],
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,111 +4,135 @@ import { CategoryType, CreateShoppingType, ProductCardType, ProductDescType, Pro
 import { prisma } from "./db";
 
 export const getProductsCard = async (): Promise<ProductCardType[]> => {
-  const products = await prisma.product.findMany({
-    select: {
-      id: true,
-      name: true,
-      price: true,
-      image: true,
-    },
-  });
-  return products;
-};
-
-export const getProductVariantImages = async (id: string) => {
-  const product = await prisma.product.findUnique(
-    {
-      where: { id },
-      select: {
-        Variant: {
-          select: {
-            image: true,
-          },
-        },
-      },
-    }
-  );
-  return product?.Variant;
-}
-
-export const getProductDesc = async (id: string): Promise<ProductDescType> => {
-  const product = await prisma.product.findUnique(
-    {
-      where: { id },
-      select: {
-        id: true,
-        name: true,
-        price: true,
-        description: true,
-        Variant: {
-          select: {
-            id: true,
-            color: true,
-            image: true,
-            Size: {
-              select: {
-                id: true,
-                name: true,
-                stock: true,
-                order: true
-              }
-            }
-          },
-        }
-      },
-    }
-  );
-
-  if (!product) throw new Error("Produto não encontrado.");
-  return product
-}
-
-export const getCategories = async (): Promise<CategoryType[]> => {
-  const category = await prisma.category.findMany(
-    {
-      select: {
-        name: true,
-      },
-    }
-  );
-  return category
-}
-
-export const getProductsFiltered = async (query?: string, category?: string, price?: string): Promise<ProductFiltered[]> => {
-  const product = await prisma.product.findMany(
-    {
-      where: {
-        OR: [
-          { name: { contains: query || "", mode: "insensitive" } },
-          { ProductCategories: {some: {categoryName: { contains: query || "", mode: "insensitive" } }} },
-          { description: { contains: query || "", mode: "insensitive" } },
-        ],
-        ProductCategories: {
-          some: {
-            categoryName: {
-              equals: category === "tudo" ? undefined : category || undefined,
-            }
-          }
-        }
-      },
+  try {
+    const products = await prisma.product.findMany({
       select: {
         id: true,
         name: true,
         price: true,
         image: true,
-        description: true
       },
-      orderBy: {
-        price: price === "asc" ? "asc" : "desc",
-      },
-    }
-  );
+    });
+    return products;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar produtos.");
+  }
+};
 
-  return product;
+export const getProductVariantImages = async (id: string) => {
+  try {
+    const product = await prisma.product.findUnique(
+      {
+        where: { id },
+        select: {
+          Variant: {
+            select: {
+              image: true,
+            },
+          },
+        },
+      }
+    );
+    return product?.Variant;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar produtos.");
+  }
+}
+
+export const getProductDesc = async (id: string): Promise<ProductDescType> => {
+  try {
+    const product = await prisma.product.findUnique(
+      {
+        where: { id },
+        select: {
+          id: true,
+          name: true,
+          price: true,
+          description: true,
+          Variant: {
+            select: {
+              id: true,
+              color: true,
+              image: true,
+              Size: {
+                select: {
+                  id: true,
+                  name: true,
+                  stock: true,
+                  order: true
+                }
+              }
+            },
+          }
+        },
+      }
+    );
+
+    if (!product) throw new Error("Produto não encontrado.");
+    return product
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar produtos.");
+  }
+}
+
+export const getCategories = async (): Promise<CategoryType[]> => {
+  try {
+    const category = await prisma.category.findMany(
+      {
+        select: {
+          name: true,
+        },
+      }
+    );
+    return category
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar categorias.");
+  }
+}
+
+export const getProductsFiltered = async (query?: string, category?: string, price?: string): Promise<ProductFiltered[]> => {
+  try {
+    const product = await prisma.product.findMany(
+      {
+        where: {
+          OR: [
+            { name: { contains: query || "", mode: "insensitive" } },
+            { ProductCategories: { some: { categoryName: { contains: query || "", mode: "insensitive" } } } },
+            { description: { contains: query || "", mode: "insensitive" } },
+          ],
+          ProductCategories: {
+            some: {
+              categoryName: {
+                equals: category === "tudo" ? undefined : category || undefined,
+              }
+            }
+          }
+        },
+        select: {
+          id: true,
+          name: true,
+          price: true,
+          image: true,
+          description: true
+        },
+        orderBy: {
+          price: price === "asc" ? "asc" : "desc",
+        },
+      }
+    );
+
+    return product;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar produtos.");
+  }
 }
 
 export const createShopping = async (data: CreateShoppingType) => {
-
   try {
     await prisma.$transaction(async (tx) => {
       async function transaction(product: { sizeId: string, quantity: number }) {
@@ -169,120 +193,155 @@ export const getSizeStock = async (id: string) => {
 }
 
 export const getSales = async () => {
-  const sales = await prisma.sale.findMany(
-    {
+  try {
+    const sales = await prisma.sale.findMany(
+      {
+        select: {
+          id: true,
+          userId: true,
+          total: true,
+          createdAt: true,
+          User: {
+            select: {
+              name: true,
+              image: true,
+            },
+          }
+        },
+      }
+    );
+    return sales;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar vendas.");
+  }
+}
+
+export const getSalesCards = async () => {
+  try { 
+    const sales = await prisma.sale.findMany({
       select: {
         id: true,
         userId: true,
         total: true,
+      },
+    });
+    return sales;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar vendas.");
+  }
+}
+
+export const getSalesDate = async () => {
+  try { 
+    const sales = await prisma.sale.findMany({
+      select: {
+        total: true,
+        createdAt: true,
+      },
+    });
+    return sales;
+  } catch (error) { 
+    console.log(error);
+    throw new Error("Erro ao buscar vendas.");
+  }
+}
+
+export const getLastSales = async () => {
+  try {
+    const sales = await prisma.sale.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 5,
+      select: {
+        id: true,
+        userId: true,
+        total: true,
+        User: {
+          select: {
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
+    return sales;
+   } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar vendas.");
+  }
+}
+
+export const getSalesPaginated = async (page: number, query?: string) => {
+  try {
+    const sales = await prisma.sale.findMany({
+      where: {
+        OR: [
+          { User: { name: { contains: query || "", mode: "insensitive" } } },
+          { userId: { contains: query || "", mode: "insensitive" } }
+        ]
+      },
+      skip: (page - 1) * 5,
+      take: 5,
+      select: {
+        id: true,
+        userId: true,
+        total: true,
+        payment_method: true,
+        delivery_method: true,
         createdAt: true,
         User: {
           select: {
             name: true,
             image: true,
           },
-        }
-      },
-    }
-  );
-  return sales;
-}
-
-export const getSalesCards = async () => {
-  const sales = await prisma.sale.findMany({
-    select: {
-      id: true,
-      userId: true,
-      total: true,
-    },
-  });
-  return sales;
-}
-
-export const getSalesDate = async () => {
-  const sales = await prisma.sale.findMany({
-    select: {
-      total: true,
-      createdAt: true,
-    },
-  });
-  return sales;
-}
-
-export const getLastSales = async () => {
-  const sales = await prisma.sale.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-    take: 5,
-    select: {
-      id: true,
-      userId: true,
-      total: true,
-      User: {
-        select: {
-          name: true,
-          image: true,
         },
-      },
-    },
-  });
-  return sales;
-}
-
-export const getSalesPaginated = async (page: number, query?: string) => {
-  const sales = await prisma.sale.findMany({
-    where: {
-      OR: [
-        { User: { name: { contains: query || "", mode: "insensitive" } } },
-        { userId: { contains: query || "", mode: "insensitive" } }
-      ]
-    },
-    skip: (page - 1) * 5,
-    take: 5,
-    select: {
-      id: true,
-      userId: true,
-      total: true,
-      payment_method: true,
-      delivery_method: true,
-      createdAt: true,
-      User: {
-        select: {
-          name: true,
-          image: true,
-        },
-      },
-    }
-  });
-  return sales;
+      }
+    });
+    return sales;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar vendas.");
+  }
 }
 
 export const getNumberOfPages = async (query?: string) => {
-  const salesCount = await prisma.sale.count({
-    where: {
-      OR: [
-        { User: { name: { contains: query || "", mode: "insensitive" } } },
-        { userId: { contains: query || "", mode: "insensitive" } }
-      ]
-    }
-  });
-  return Math.ceil(salesCount / 5);
+  try {
+    const salesCount = await prisma.sale.count({
+      where: {
+        OR: [
+          { User: { name: { contains: query || "", mode: "insensitive" } } },
+          { userId: { contains: query || "", mode: "insensitive" } }
+        ]
+      }
+    });
+    return Math.ceil(salesCount / 5);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar vendas.");
+  }
 }
 
 export const getRelatedProducts = async (id: string) => {
-  const products = await prisma.product.findMany({
-    where: {
-      id: {
-        not: id,
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        id: {
+          not: id,
+        },
       },
-    },
-    select: {
-      id: true,
-      name: true,
-      price: true,
-      image: true,
-    },
-  });
-  return products;
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        image: true,
+      },
+    });
+    return products;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao buscar produtos.");
+  }
 }
